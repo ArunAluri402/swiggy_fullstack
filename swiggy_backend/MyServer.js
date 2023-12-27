1bef5ee6df88eb3ec5043d30e8bb195753ce4ece
ArunAluri402 <arun.prakash421@gmail.com>
2023-12-27 18:43:32 +0530

    added chenges

--- swiggy_backend/MyServer.js
+++ swiggy_backend/MyServer.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4194;
 const mysql = require("mysql2");
-app.use(express.json()); // Enable JSON body parsing
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -28,8 +28,6 @@ db.connect((err) => {
   }
 });
 
-// You can use 'db' to perform database operations
-
 app.post("/api/v1/registration", (req, res) => {
   const { userId, phoneNo, type, email } = req.body;
 
@@ -37,20 +35,78 @@ app.post("/api/v1/registration", (req, res) => {
 
   const currentDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
+  const queryCheckDuplicates =
+    "SELECT COUNT(*) as count FROM swiggy_fullstack.user WHERE phone_no = ? OR email = ?";
+  const valuesCheckDuplicates = [phoneNo, email];
+
+  db.query(
+    queryCheckDuplicates,
+    valuesCheckDuplicates,
+    (err, duplicateResults) => {
+      if (err) {
+        console.error("Error checking for duplicates in the database:", err);
+        res
+          .status(500)
+          .json({ success: false, error: "Internal Server Error" });
+      } else {
+        const duplicateCount = duplicateResults[0].count;
+
+        if (duplicateCount > 0) {
+          res.status(409).json({
+            success: false,
+            error: "User with the same phone number or email already exists",
+          });
+        } else {
+          const query =
+            "INSERT INTO swiggy_fullstack.user (user_id, phone_no, type, email, last_login) VALUES (?, ?, ?, ?, ?)";
+          const values = [userId, phoneNo, type, email, currentDate];
+
+          db.query(query, values, (err, results) => {
+            if (err) {
+              console.error("Error inserting user into the database:", err);
+              res
+                .status(500)
+                .json({ success: false, error: "Internal Server Error" });
+            } else {
+              console.log("User inserted into the database");
+              res.status(200).json({
+                success: true,
+                message: "User successfully registered",
+              });
+            }
+          });
+        }
+      }
+    }
+  );
+});
+
+app.post("/api/v1/valLogin", (req, res) => {
+  const { phoneNo, email } = req.body;
+
   const query =
-    "INSERT INTO swiggy_fullstack.user (user_id, phone_no, type, email, last_login) VALUES (?, ?, ?, ?, ?)";
-  const values = [userId, phoneNo, type, email, currentDate];
+    "SELECT * FROM swiggy_fullstack.user WHERE phone_no = ? and email=?";
+  const values = [phoneNo, email];
 
   db.query(query, values, (err, results) => {
     if (err) {
-      console.error("Error inserting user into the database:", err);
+      console.error("Error querying the database:", err);
       res.status(500).json({ success: false, error: "Internal Server Error" });
     } else {
-      console.log("User inserted into the database");
-      res.status(200).json({
-        success: true,
-        message: "User successfully registered",
-      });
+      if (results.length > 0) {
+        res.status(200).json({
+          success: true,
+          response_code: 200,
+          response_message: "user exists in the database",
+          response_dtls: results[0],
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          response_code: 404,
+          response_message: "user not found in the database",
+        });
+      }
     }
   });
 });
